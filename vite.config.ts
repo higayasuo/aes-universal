b065051630e8eaf07492b15eb9c90113df828d30
Yasuo Higa <higayasuo@gmail.com>
2025-04-18 15:24:22 +0900

    Remove 'node-forge' from external dependencies in vite.config.ts to streamline build configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       fileName: (format) => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
     },
     rollupOptions: {
-      external: ['node-forge', 'expo-crypto-universal'],
+      external: ['expo-crypto-universal'],
       output: {
         globals: {},
         interop: 'auto',
